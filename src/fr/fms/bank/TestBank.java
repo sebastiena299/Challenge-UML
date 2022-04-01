4da07eee12745d6cb1373dedcef74ba0436c7959
Sebastien Allaire <sebastien.allaire.fms@gmail.com>
2022-04-01 10:06:20 +0200

    :tada: Update tests

--- src/fr/fms/bank/TestBank.java
+++ src/fr/fms/bank/TestBank.java
@@ -1,7 +1,7 @@
 package fr.fms.bank;
 
-import java.util.Scanner;
-
+import java.util.ArrayList;
+import fr.fms.entities.Account;
 import fr.fms.entities.Admin;
 import fr.fms.entities.Client;
 import fr.fms.entities.CurrentAccount;
@@ -9,29 +9,36 @@ import fr.fms.entities.SavingAccount;
 
 public class TestBank {
 	
-	public Scanner scan = new Scanner(System.in);
-	int userClientNumber;
-	
-	public void authentification() {
-		
-		System.out.println("Entrez votre numéro client");
-		while(!scan.hasNextInt()) scan.next();
-		userClientNumber = scan.nextInt();
-		
-	}
+//	public Scanner scan = new Scanner(System.in);
+//	int userClientNumber;
+//	
+//	public void authentification() {
+//		System.out.println("Entrez votre numéro client");
+//		while(!scan.hasNextInt()) scan.next();
+//		userClientNumber = scan.nextInt();	
+//	}
 
 
 	public static void main(String[] args) {
 		
+		ArrayList<Client> clients = new ArrayList<>();
+		ArrayList<Account> accounts = new ArrayList<>();
+		
 		Client test = new Client(1, 44848, "Doe", "John", "Dax");
-		
 		Admin admin =  new Admin(1, 12, "Doe", "John", "Paris");
-		
 		Client totoClient = admin.createClient(2, 1654, "Fix", "Mathieu", "Dax");
+		clients.add(test);
+		clients.add(admin);
+		clients.add(totoClient);
 		
 		CurrentAccount compte = admin.createCurrentAccount(40,totoClient.getClientNumber(),0,-200);
-		
 		SavingAccount epargne = new SavingAccount(41,totoClient.getClientNumber(),0,1);
+		accounts.add(compte);
+		accounts.add(epargne);
+		
+		accounts.stream()
+			.filter(client -> client.getClientNumber() == 1654)
+			.forEach(account -> account.consultation());
 	
 		System.out.println(test);
 		System.out.println(admin);
