57840c877121b2f0eebf2fa7c0031a5e69abf9b6
EricM <101633226+EricMauler@users.noreply.github.com>
2022-04-04 15:34:37 +0200

    IJOb

--- src/fr/fms/entities/Client.java
+++ src/fr/fms/entities/Client.java
@@ -92,6 +92,8 @@ public class Client {
 		return isAdmin;
 	}
 	
+	
+	
 	// Méthodes
 	
 	public String displayDate() {
--- src/fr/fms/interfaces/IJobOperationImpl.java
+++ src/fr/fms/interfaces/IJobOperationImpl.java
@@ -92,3 +92,4 @@ public class IJobOperationImpl implements IJobOperation {
 	}}
 
 
+
